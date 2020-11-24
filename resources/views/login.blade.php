5b7d0245d53bfb7e64ed41a3fcafa10bd8ed4f49
Pritam Hirve <hiravepritam@gmail.com>
2020-11-24 23:53:44 +0530

    csrf token

---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+<h1>User Login</h1>
+@csrf
+<form action="user" method="get">
+    <input type="text" name="user" placeholder="Enter user name"> <br></br>
+    <input type="password" name="password" placeholder="Enter user password"> <br></br>
+    <button type="submit">Login</button>
+</form>
