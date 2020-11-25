3444965521eda444ff3ba399ed496cc1167e5ce0
Pritam Hirve <hiravepritam@gmail.com>
2020-11-25 22:33:12 +0530

    csrf

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,4 @@
 <h1>User Login</h1>
-@csrf
 <form action="user" method="get">
     <input type="text" name="user" placeholder="Enter user name"> <br></br>
     <input type="password" name="password" placeholder="Enter user password"> <br></br>
